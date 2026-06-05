fd17b7d37e18bd029fa2fffb592603738f852db2
defect122 <274657713+defect122@users.noreply.github.com>
2026-06-05 15:17:59 -0700

    Removed unecessary files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-import { createTodoList } from "./table";
-import { openModal, pressedModal } from "./popup";
-
 if (process.env.NODE_ENV !== "production") {
   console.log("Looks like we are in development mode!");
 }
-
-createTodoList();
-pressedModal();
